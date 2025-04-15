a843f4dfebd7bb90d1029966e7bc0229970fc1bc
Kevin Llanos <kllano2@wgu.edu>
2025-04-15 14:33:10 -0500

    inventoryservice finish and working

---
+++ inventoryservice/src/main/java/com/kevinll/inventoryservice/response/EventInventoryResponse.java
@@ -0,0 +1,22 @@
+package com.kevinll.inventoryservice.response;
+
+
+import com.kevinll.inventoryservice.entity.Venue;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventInventoryResponse {
+    private Long eventId;
+    private String event;
+    private Long capacity;
+    private Venue venue;
+    private BigDecimal ticketPrice;
+}
